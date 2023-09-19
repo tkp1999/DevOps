88f3e84c232827871ff6cd09a3a8e0b36f2ad6ad
Tarun Kumar Pal <tarunkumarpal468@gmail.com>
2023-09-19 11:23:35 +0530

    created multiple vpc in aws subnet

--- terraform/aws/n-tier-aws/inputs.tf
+++ terraform/aws/n-tier-aws/inputs.tf
@@ -4,8 +4,21 @@ variable "vpc_network_cidr" {
   description = "This is aws vpc network cidr block"
 
 }
+/*
 variable "web1subnetcidr" {
   type        = string
   default     = "10.10.0.0/24"
   description = "This is web1 subnet cidr"
+}
+*/
+variable "aws_subnet_cidr_ranges" {
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.30.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+  description = "these are aws subnet ranges"
+}
+variable "aws_subnet_names" {
+  type        = list(string)
+  default     = ["app1", "app2", "web1", "web2", "db1", "db2"]
+  description = "These are default values of subnet names"
+
 }
--- terraform/aws/n-tier-aws/main.tf
+++ terraform/aws/n-tier-aws/main.tf
@@ -5,11 +5,22 @@ resource "aws_vpc" "ntier" {
   }
 }
 
+/*
 resource "aws_subnet" "web1" {
   tags = {
     Name = "web1"
   }
   vpc_id     = aws_vpc.ntier.id
   cidr_block = var.web1subnetcidr
+  depends_on = [aws_vpc.ntier]
+}
+*/
+resource "aws_subnet" "ntier_subnet" {
+  count = length(var.aws_subnet_cidr_ranges)
+  tags = {
+    Name = var.aws_subnet_names[count.index]
+  }
+  vpc_id     = aws_vpc.ntier.id
+  cidr_block = var.aws_subnet_cidr_ranges[count.index]
   depends_on = [aws_vpc.ntier]
 }
